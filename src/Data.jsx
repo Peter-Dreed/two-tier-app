4dbf3bfb216312eaff1f544e73331dc1c6e9be84
GOMA PETER ORAH <peterorah40@gmail.com>
2025-09-26 23:30:47 +0100

    Update fetch URL in Data component

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,7 +4,7 @@ function Data() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://34.254.239.184:5000/data")
+    fetch("http://44.210.141.95/:5000/data")
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
